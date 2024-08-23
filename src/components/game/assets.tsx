67ffcd44d06f7b1ee10f50232f5fd9502751d113
Chau Nguyen <96738596+chaunguyenm@users.noreply.github.com>
2024-08-23 10:08:45 -0400

    added game assets

---
+++ src/components/game/assets.tsx
@@ -0,0 +1,81 @@
+// https://github.com/berthutapea/mario-jump-game
+
+export function DinoStanding() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="66" viewBox="0 0 20 22" version="1.1">
+      <path d="M 11 0, L 19 0, L 19 1, L 20 1, L 20 6, L 15 6, L 15 7, L 18 7,
+         L 18 8, L 14 8, L 14 10, L 16 10, L 16 12, L 15 12, L 15 11, L 14 11,
+         L 14 14, L 13 14, L 13 16, L 12 16, L 12 17, L 11 17, L 11 21, L 12 21,
+         L 12 22, L 10 22, L 10 19, L 9 19, L 9 18, L 8 18,
+         L 8 19, L 7 19, L 7 20, L 6 20, L 6 21, L 7 21, L 7 22, L 5 22, L 5 18,
+         L 4 18, L 4 17, L 3 17, L 3 16, L 2 16, L 2 15, L 1 15, L 1 14, L 0 14,
+         L 0 8, L 1 8, L 1 10, L 2 10, L 2 11, L 3 11, L 3 12, L 5 12, L 5 11,
+         L 6 11, L 6 10, L 8 10, L 8 9, L 9 9, L 9 8, L 10 8, L 10 1, L 11 1, L 11 0"
+        className="stroke-none fill-black dark:fill-white" />
+      <path d="M 12 2, L 13 2, L 13 3, L 12 3, L 12 2" className="stroke-none fill-white dark:fill-black" />
+    </svg>
+  );
+}
+
+export function DinoDead() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="66" viewBox="0 0 20 22" version="1.1">
+      <path d="M 11 0, L 19 0, L 19 1, L 20 1, L 20 6, L 18 6, L 18 7, L 14 7, L 14 10, L 16 10,
+         L 16 12, L 15 12, L 15 11, L 14 11, L 14 14, L 13 14, L 13 16, L 12 16, L 12 17, L 11 17,
+         L 11 21, L 12 21, L 12 22, L 10 22, L 10 19, L 9 19, L 9 18, L 8 18,
+         L 8 19, L 7 19, L 7 20, L 6 20, L 6 21, L 7 21, L 7 22, L 5 22, L 5 18,
+         L 4 18, L 4 17, L 3 17, L 3 16, L 2 16, L 2 15, L 1 15, L 1 14, L 0 14,
+         L 0 8, L 1 8, L 1 10, L 2 10, L 2 11, L 3 11, L 3 12, L 5 12, L 5 11,
+         L 6 11, L 6 10, L 8 10, L 8 9, L 9 9, L 9 8, L 10 8, L 10 1, L 11 1, L 11 0"
+        className="stroke-none fill-black dark:fill-white" />
+      <path d="M 11.5 1.5, L 13.5 1.5, L 13.5 3.5, L 11.5 3.5, L 11.5 1.5" className="stroke-none fill-white dark:fill-black" />
+      <path d="M 12 2, L 13 2, L 13 3, L 12 3, L 12 2" className="stroke-none fill-black dark:fill-white" />
+    </svg>
+  );
+}
+
+export function DinoRunningLeft() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="66" viewBox="0 0 20 22" version="1.1">
+      <path d="M 11 0, L 19 0, L 19 1, L 20 1, L 20 6, L 15 6, L 15 7, L 18 7,
+         L 18 8, L 14 8, L 14 10, L 16 10, L 16 12, L 15 12, L 15 11, L 14 11,
+         L 14 14, L 13 14, L 13 16, L 12 16, L 12 17, L 11 17, L 11 21, L 12 21,
+         L 12 22, L 10 22, L 10 19, L 9 19, L 9 18, L 7 18,
+         L 7 19, L 8 19, L 8 20, L 6 20, L 6 19, L 5 19, L 5 18,
+         L 4 18, L 4 17, L 3 17, L 3 16, L 2 16, L 2 15, L 1 15, L 1 14, L 0 14,
+         L 0 8, L 1 8, L 1 10, L 2 10, L 2 11, L 3 11, L 3 12, L 5 12, L 5 11,
+         L 6 11, L 6 10, L 8 10, L 8 9, L 9 9, L 9 8, L 10 8, L 10 1, L 11 1, L 11 0"
+        className="stroke-none fill-black dark:fill-white" />
+      <path d="M 12 2, L 13 2, L 13 3, L 12 3, L 12 2" className="stroke-none fill-white dark:fill-black" />
+    </svg>
+  );
+}
+
+export function DinoRuningRight() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="66" viewBox="0 0 20 22" version="1.1">
+      <path d="M 11 0, L 19 0, L 19 1, L 20 1, L 20 6, L 15 6, L 15 7, L 18 7,
+         L 18 8, L 14 8, L 14 10, L 16 10, L 16 12, L 15 12, L 15 11, L 14 11,
+         L 14 14, L 13 14, L 13 16, L 12 16, L 12 17, L 11 17,
+         L 11 18, L 12 18, L 12 19, L 10 19, L 10 18, L 8 18,
+         L 8 19, L 7 19, L 7 20, L 6 20, L 6 21, L 7 21, L 7 22, L 5 22, L 5 18,
+         L 4 18, L 4 17, L 3 17, L 3 16, L 2 16, L 2 15, L 1 15, L 1 14, L 0 14,
+         L 0 8, L 1 8, L 1 10, L 2 10, L 2 11, L 3 11, L 3 12, L 5 12, L 5 11,
+         L 6 11, L 6 10, L 8 10, L 8 9, L 9 9, L 9 8, L 10 8, L 10 1, L 11 1, L 11 0"
+        className="stroke-none fill-black dark:fill-white" />
+      <path d="M 12 2, L 13 2, L 13 3, L 12 3, L 12 2" className="stroke-none fill-white dark:fill-black" />
+    </svg>
+  );
+}
+
+export function Cloud() {
+  return (
+    <svg width="94" height="28" viewBox="0 0 47 14" fill="none">
+      <path d="M 0 14, L 2 14, L 2 13, L 3 13, L 3 12, L 1 12, L 1 13, L 0 13, L 0 14" stroke="none" fill="grey" />
+      <path d="M 4 13, L 4 12, L 5 12, L 5 9, L 6 9, L 6 8, L 14 8, L 14 7, L 15 7, L 15 6, L 18 6, L 18 3, L 20 3, L 20 2, L 21 2, L 21 1, L 26 1, L 26 0, L 30 0, L 30 1, L 32 1, L 32 3, L 33 3, L 33 5, L 36 5, L 36 6, L 41 6, L 41 8, L 44 8, L 44 10, L 46 10, L 46 12, L 45 12, L 45 11, L 43 11, L 43 9, L 40 9, L 40 7, L 35 7, L 35 6, L 33 6, L 33 7, L 32 7, L 32 4, L 31 4, L 31 2, L 29 2, L 29 1, L 27 1, L 27 2, L 22 2, L 22 3, L 21 3, L 21 4, L 19 4, L 19 7, L 16 7, L 16 8, L 15 8, L 15 9, L 7 9, L 7 10, L 6 10, L 6 13, L 4 13" stroke="none" fill="grey" />
+      <path d="M 31 7, L 32 7, L 32 8, L 31 8, L 31 7" stroke="none" fill="grey" />
+      <path d="M 9 12, L 10 12, L 10 13, L 9 13, L 9 12" stroke="none" fill="grey" />
+      <path d="M 46 12, L 47 12, L 47 14, L 10 14, L 10 13, L 46 13, L 46 12" stroke="none" fill="grey" />
+    </svg>
+  );
+}
